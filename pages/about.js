72eb6437fbd63bf6d88b7a8389577a40ce8bc11e
hanjuuuuuu <hanjucomss@naver.com>
2022-11-10 17:02:22 +0900

    redirect and rewrite

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
-import NavBar from "../component/NavBar";
+import Seo from "../component/Seo";
 
 export default function Potato() {
-    return <div>
-        <NavBar />
+    return (
+    <div>
+        <Seo title="About"/>
         <h1>About</h1>
     </div>
+    );
 }
